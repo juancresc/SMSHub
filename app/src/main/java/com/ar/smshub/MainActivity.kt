042ffc0533f0eac0bb9bdac3e0d1fca43169e291
juan <juan.crescente@gmail.com>
2019-04-23 20:58:10 -0300

    initial commit

--- app/src/main/java/com/ar/smshub/MainActivity.kt
+++ app/src/main/java/com/ar/smshub/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.ar.smshub
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -18,17 +18,32 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
+        super.onCreateOptionsMenu(menu)
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
+
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_settings -> {
+                msgShow("Setting")
+                val firstFragment = FragmentSettings()
+                firstFragment.arguments = intent.extras
+                val transaction = fragmentManager.beginTransaction()
+                transaction.replace(R.id.main_view, firstFragment)
+                transaction.commit()
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    fun msgShow(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
     }
 }
