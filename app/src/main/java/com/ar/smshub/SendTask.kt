b73f43b60c6375198d86fe6f21a502851d858848
ampped101 <ampped101@gmail.com>
2019-08-16 11:53:54 -0400

    Accept A JSON array
    [ { "message":"Message 1", "number":"1234567890", "messageId":"1" }, {	"message":"Message 2", "number":"1234567890", "messageId":"2" }, { "message":"Message 3", "number":"1234567890", "messageId":"3" } ]

--- app/src/main/java/com/ar/smshub/SendTask.kt
+++ app/src/main/java/com/ar/smshub/SendTask.kt
@@ -29,10 +29,11 @@ class SendTask constructor(_settings: SettingsManager, _context: Context) : Time
             Log.d("-->", "Cannot connect to URL")
             return
         }
-        var sms: SMS? = SMS("", "", "")
+        //var sms: SMS? = SMS("", "", "")
+        var smsArray: List<SMS>? = emptyList()
         var canSend: Boolean = false
         try {
-            sms = Klaxon().parse<SMS>(apiResponse.text)
+            smsArray = Klaxon().parseArray<SMS>(apiResponse.text)
             canSend = true
         } catch (e: com.beust.klaxon.KlaxonException) {
             if (apiResponse.text == "") {
@@ -50,32 +51,35 @@ class SendTask constructor(_settings: SettingsManager, _context: Context) : Time
             // optional finally block
         }
         if (canSend) {
-            val sentIn = Intent(mainActivity.SENT_SMS_FLAG)
-            settings.updateSettings()
-            sentIn.putExtra("messageId", sms!!.messageId)
-            sentIn.putExtra("statusURL", settings.statusURL)
-            sentIn.putExtra("deviceId", settings.deviceId)
-            sentIn.putExtra("delivered", 0)
+            smsArray?.forEach {
+                val sentIn = Intent(mainActivity.SENT_SMS_FLAG)
+                settings.updateSettings()
+                sentIn.putExtra("messageId", it!!.messageId)
+                sentIn.putExtra("statusURL", settings.statusURL)
+                sentIn.putExtra("deviceId", settings.deviceId)
+                sentIn.putExtra("delivered", 0)
 
 
-            val sentPIn = PendingIntent.getBroadcast(mainActivity, mainActivity.nextRequestCode(), sentIn,0)
+                val sentPIn = PendingIntent.getBroadcast(mainActivity, mainActivity.nextRequestCode(), sentIn,0)
 
-            val deliverIn = Intent(mainActivity.DELIVER_SMS_FLAG)
-            deliverIn.putExtra("messageId", sms!!.messageId)
-            deliverIn.putExtra("statusURL", settings.statusURL)
-            deliverIn.putExtra("deviceId", settings.deviceId)
-            deliverIn.putExtra("delivered", 1)
+                val deliverIn = Intent(mainActivity.DELIVER_SMS_FLAG)
+                deliverIn.putExtra("messageId", it!!.messageId)
+                deliverIn.putExtra("statusURL", settings.statusURL)
+                deliverIn.putExtra("deviceId", settings.deviceId)
+                deliverIn.putExtra("delivered", 1)
 
 
-            val deliverPIn = PendingIntent.getBroadcast(mainActivity, mainActivity.nextRequestCode(), deliverIn, 0)
+                val deliverPIn = PendingIntent.getBroadcast(mainActivity, mainActivity.nextRequestCode(), deliverIn, 0)
 
-            val smsManager = SmsManager.getDefault() as SmsManager
-            smsManager.sendTextMessage(sms!!.number, null, sms!!.message, sentPIn, deliverPIn)
-            mainActivity.runOnUiThread(Runnable {
-                mainActivity.logMain("Sent to: " + sms!!.number + " - id: " + sms!!.messageId + " - message: " + sms!!.message)
-            })
-            Log.d("-->", "Sent!")
+                val smsManager = SmsManager.getDefault() as SmsManager
+                smsManager.sendTextMessage(it!!.number, null, it!!.message, sentPIn, deliverPIn)
+                mainActivity.runOnUiThread(Runnable {
+                    mainActivity.logMain("Sent to: " + it!!.number + " - id: " + it!!.messageId + " - message: " + it!!.message)
+                })
+                Log.d("-->", "Sent!")
 
+                Thread.sleep(500)
+            }
         }
 
 
