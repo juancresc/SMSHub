6d8f70caaa3214ea78712209a99766eb1f7d01ca
Luigi Taira <Luigi123@users.noreply.github.com>
2021-07-12 13:47:26 +0900

    メッセージを分割して送る様に修正

--- app/src/main/java/com/ar/smshub/SendTask.kt
+++ app/src/main/java/com/ar/smshub/SendTask.kt
@@ -6,8 +6,8 @@ import android.content.Intent
 import android.telephony.SmsManager
 import android.util.Log
 import com.beust.klaxon.Klaxon
-import java.util.*
 import khttp.responses.Response
+import java.util.*
 
 class SMS(val message: String, val number: String, val messageId: String)
 
@@ -59,20 +59,24 @@ class SendTask constructor(_settings: SettingsManager, _context: Context) : Time
                 sentIn.putExtra("deviceId", settings.deviceId)
                 sentIn.putExtra("delivered", 0)
 
-
-                val sentPIn = PendingIntent.getBroadcast(mainActivity, mainActivity.nextRequestCode(), sentIn,0)
-
                 val deliverIn = Intent(mainActivity.DELIVER_SMS_FLAG)
                 deliverIn.putExtra("messageId", it!!.messageId)
                 deliverIn.putExtra("statusURL", settings.statusURL)
                 deliverIn.putExtra("deviceId", settings.deviceId)
                 deliverIn.putExtra("delivered", 1)
 
-
-                val deliverPIn = PendingIntent.getBroadcast(mainActivity, mainActivity.nextRequestCode(), deliverIn, 0)
-
                 val smsManager = SmsManager.getDefault() as SmsManager
-                smsManager.sendTextMessage(it!!.number, null, it!!.message, sentPIn, deliverPIn)
+
+                val parts: ArrayList<String> = smsManager.divideMessage(it!!.message)
+                val sentIntents = ArrayList<PendingIntent>()
+                val deliveryIntents = ArrayList<PendingIntent>()
+                for (i in 0 until parts.size) {
+                    val sentPIn = PendingIntent.getBroadcast(mainActivity, mainActivity.nextRequestCode(), sentIn,0)
+                    val deliverPIn = PendingIntent.getBroadcast(mainActivity, mainActivity.nextRequestCode(), deliverIn, 0)
+                    sentIntents.add(sentPIn)
+                    deliveryIntents.add(deliverPIn)
+                }
+                smsManager.sendMultipartTextMessage(it!!.number, null, parts, sentIntents, deliveryIntents)
                 mainActivity.runOnUiThread(Runnable {
                     mainActivity.logMain("Sent to: " + it!!.number + " - id: " + it!!.messageId + " - message: " + it!!.message)
                 })
